110a55f2a84bd6432859e14f2018173d1e3e5396
abhisanGhimire <abhisanghimire@dr-ait.org>
2020-09-11 01:56:05 +0545

    lazy_learner

--- main.js
+++ main.js
@@ -1,30 +1,93 @@
-var app = new Vue({
-	el: "#app",
-	data: {
-		product: "Socks",
-		image: "./greenSocks.jpg",
-		inStock: false,
-		details: ["80% cotton", "20% polyester", "Gender-neutral"],
-		variants: [
-			{
-				variantId: 1,
-				variantColor: "green",
-				variantImage: "./greenSocks.jpg",
-			},
-			{
-				variantId: 2,
-				variantColor: "blue",
-				variantImage: "./blueSocks.jpg",
-			},
-		],
-		cart: 0,
+Vue.component("product", {
+	props: {
+		premium: {
+			type: Boolean,
+			required: true,
+		},
+	},
+	template: `<div class="product">
+	<div class="product-image">
+		<img :src="image" />
+	</div>
+	<div class="product-info">
+		<h1>{{ title }}</h1>
+		<p v-if="inStock">In Stock</p>
+		<p v-else>Out of Stock</p>
+		<p>Shipping:{{shipping}}</p>
+		<ul>
+			<li v-for="detail in details">{{ detail }}</li>
+		</ul>
+		<div
+			v-for="(variant,index) in variants"
+			:key="variant.variantId"
+			class="color-box"
+			:style="{backgroundColor:variant.variantColor}"
+			@mouseover="updateProduct(index)"
+		></div>
+		<button
+			v-on:click="addToCart"
+			:disabled="!inStock"
+			:class="{disabledButton:!inStock}"
+		>
+			Add to Cart
+		</button>
+		<div class="cart">
+			<p>Cart({{ cart }})</p>
+		</div>
+	</div>
+</div>`,
+	data() {
+		return {
+			brand: "Invictus",
+			product: "Socks",
+			selectedVariant: 0,
+			details: ["80% cotton", "20% polyester", "Gender-neutral"],
+			variants: [
+				{
+					variantId: 1,
+					variantColor: "green",
+					variantImage: "./greenSocks.jpg",
+					variantQuantity: 10,
+				},
+				{
+					variantId: 2,
+					variantColor: "blue",
+					variantImage: "./blueSocks.jpg",
+					variantQuantity: 0,
+				},
+			],
+			cart: 0,
+		};
 	},
 	methods: {
 		addToCart() {
 			this.cart += 1;
 		},
-		updateProduct(variantImage) {
-			this.image = variantImage;
+		updateProduct(index) {
+			this.selectedVariant = index;
 		},
+	},
+	computed: {
+		title() {
+			return this.brand + " " + this.product;
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity;
+		},
+		shipping() {
+			if (this.premium) {
+				return "Free";
+			}
+			return "$2.99";
+		},
+	},
+});
+var app = new Vue({
+	el: "#app",
+	data: {
+		premium: true,
 	},
 });
